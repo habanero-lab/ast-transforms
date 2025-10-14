618dff007867009f68222845fff244ac4a09bb41
Tong Zhou <zt9465@gmail.com>
2025-10-14 10:22:01 +0800

    feat(api)!: update API to ast_transforms.name_of_the_pass(tree, *args); passes are organized into ast_transforms/passes/

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-A collection of AST-based transformations.
+A collection of AST-based transformations. Usage example:
+
+```python
+import ast
+from ast_transforms import remove_func_decorator
+
+code = '''
+@mydecorator
+def foo():
+    print("foo")
+'''
+
+tree = ast.parse(code)
+tree = remove_func_decorator(tree)  # removes all function decorators
+newcode = ast.unparse(tree)
+print(newcode)  # should print `def foo():\n    print("foo")`
+```
--- ast_transforms/__init__.py
+++ ast_transforms/__init__.py
@@ -1,9 +1,35 @@
-import inspect, importlib
+def add_func_decorator(tree, *args):
+    '''Apply the `add_func_decorator` AST transform.'''
+    from .passes import add_func_decorator as m
+    return m.transform(tree, *args)
 
-def apply_transform_on_ast(tree, m, *args):
-    assert inspect.ismodule(m) or isinstance(m, str)
-    if isinstance(m, str):
-        module_name = f"ast_transforms.{m}"
-        m = importlib.import_module(module_name)
-    tree = m.transform(tree, *args)
-    return tree
+def attach_def_use_vars(tree, *args):
+    '''Apply the `attach_def_use_vars` AST transform.'''
+    from .passes import attach_def_use_vars as m
+    return m.transform(tree, *args)
+
+def remove_func_arg_annotation(tree, *args):
+    '''Apply the `remove_func_arg_annotation` AST transform.'''
+    from .passes import remove_func_arg_annotation as m
+    return m.transform(tree, *args)
+
+def remove_func_decorator(tree, *args):
+    '''Apply the `remove_func_decorator` AST transform.'''
+    from .passes import remove_func_decorator as m
+    return m.transform(tree, *args)
+
+def replace_name(tree, *args):
+    '''Apply the `replace_name` AST transform.'''
+    from .passes import replace_name as m
+    return m.transform(tree, *args)
+
+def to_single_op_form(tree, *args):
+    '''Apply the `to_single_op_form` AST transform.'''
+    from .passes import to_single_op_form as m
+    return m.transform(tree, *args)
+
+def where_to_ternary(tree, *args):
+    '''Apply the `where_to_ternary` AST transform.'''
+    from .passes import where_to_ternary as m
+    return m.transform(tree, *args)
+
