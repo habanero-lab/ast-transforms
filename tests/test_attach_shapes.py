1d9715de3e3e3a0163fd70bf1eb430b9de3fc380
Tong Zhou <zt9465@gmail.com>
2026-01-10 18:13:55 +0800

    test: add test_subscripted_matmul3

--- tests/test_attach_shapes.py
+++ tests/test_attach_shapes.py
@@ -458,6 +458,19 @@ def test_subscripted_matmul2():
     results = [(ast.unparse(node), shape) for node, shape in shape_info.items() if isinstance(node, (ast.Subscript, ast.BinOp))]
     assert results == [('a[:i]', (':i',)), ('b[:i]', (':i',)), ('a[:i] @ b[:i]', ())]
 
+def test_subscripted_matmul3():
+    code = """
+    a[:i, :j] @ b[:j]
+    """
+    tree = ast.parse(textwrap.dedent(code))
+    rt_vals = {
+        "a": np.random.randn(10, 100),
+        "b": np.random.randn(100),
+    }
+    shape_info = shape_analysis.visit(tree, rt_vals)
+    results = [(ast.unparse(node), shape) for node, shape in shape_info.items() if isinstance(node, (ast.Subscript, ast.BinOp))]
+    assert results == [('a[:i, :j]', (':i', ':j')), ('b[:j]', (':j',)), ('a[:i, :j] @ b[:j]', (':i',))]
+
 # def test10():
 #     code = """
 #     c = a + b
