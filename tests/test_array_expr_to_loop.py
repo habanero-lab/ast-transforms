3cd8ed1ce04d371515a2d40e0ec55dd05ab59e1e
Tong Zhou <zt9465@gmail.com>
2026-01-13 20:53:23 +0800

    feat: now test_add 1 and 2 pass in test_array_expr_to_loop.py

--- tests/test_array_expr_to_loop.py
+++ tests/test_array_expr_to_loop.py
@@ -11,6 +11,45 @@ def test_add1():
     tree = ast.parse(textwrap.dedent(code))
     rt_vals = {
         'a': np.random.randn(10),
+        'b': 8,
+        'c': np.empty(10)
+    }
+    tree = array_expr_to_loop.transform(tree, rt_vals)
+
+    expected = """
+    for __i0 in range(10):
+        c[__i0] = a[__i0] + b
+    """
+    new_code = ast.unparse(tree)
+    print(new_code)
+    assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
+
+def test_add2():
+    code = """
+    c = a[:] + b
+    """
+    tree = ast.parse(textwrap.dedent(code))
+    rt_vals = {
+        'a': np.random.randn(10),
+        'b': 2,
+        'c': np.empty(10)
+    }
+    tree = array_expr_to_loop.transform(tree, rt_vals)
+
+    expected = """
+    for __i0 in range(10):
+        c[__i0] = a[__i0] + b
+    """
+    new_code = ast.unparse(tree)
+    assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
+
+def test_add3():
+    code = """
+    c[:] = a[:] + b
+    """
+    tree = ast.parse(textwrap.dedent(code))
+    rt_vals = {
+        'a': np.random.randn(10),
         'b': 2,
         'c': np.empty(10)
     }
