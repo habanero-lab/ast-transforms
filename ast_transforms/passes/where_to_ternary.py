7501dcb366a630eeb8aba4275eb5631651d59cf2
Tong Zhou <zt9465@gmail.com>
2026-01-13 15:06:37 +0800

    refactor: refactor where_to_ternary.py

--- ast_transforms/passes/where_to_ternary.py
+++ ast_transforms/passes/where_to_ternary.py
@@ -2,15 +2,22 @@ import ast
 
 class WhereToTernary(ast.NodeTransformer):
     def visit_Call(self, node):
-        if isinstance(node.func, ast.Name) and node.func.id == 'where':
-            assert len(node.args) == 3
-            newnode = ast.IfExp(
+        func = node.func
+        if (
+            (isinstance(func, ast.Name) and func.id == 'where')
+            or 
+            (isinstance(func, ast.Attribute)
+             and isinstance(func.value, ast.Name)
+             and func.attr == 'where'
+             and func.value.id in ['np', 'numpy', 'torch', 'cupy'])
+        ):
+            assert len(node.args) == 3, f"where should have 3 arguments, but got {len(node.args)}"
+            return ast.IfExp(
                 test=node.args[0],
                 body=node.args[1],
                 orelse=node.args[2],
+                lineno=node.lineno
             )
-            newnode.lineno = node.lineno
-            node = newnode
         return node
 
 def transform(node):
